52d01f5129410a69498c9ab2d3ce0a528891a33d
DongVV2 <vovandong14i1@gmail.com>
2018-05-25 23:45:09 +0700

    add

---
+++ ApacheTiles/src/main/java/edu/dongvv2/controller/HomeController.java
@@ -0,0 +1,15 @@
+package edu.dongvv2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "home";
+	}
+	
+}
